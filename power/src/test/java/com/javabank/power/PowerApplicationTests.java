88f2f6c5aef0a35cb35471570e7d637f9fb4485b
Estefanio-santos <mr.estefanio@hotmail.com>
2022-01-07 18:01:47 -0300

    adicionado pacote SPRINGBOOT e realizada atualizações

---
+++ power/src/test/java/com/javabank/power/PowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabank.power;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
