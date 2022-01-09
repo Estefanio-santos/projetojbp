bd759fca4f76c74969df5d70809e189ca8dabc69
domingosmpbrasil <domingosmp17@gmail.com>
2022-01-09 12:05:55 -0300

    atualisa DTO

---
+++ power/src/main/java/com/javabank/power/dto/BaseContaDto.java
@@ -0,0 +1,64 @@
+package com.javabank.power.dto;
+
+public class BaseContaDto {
+
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private double saldo;
+	private String titular;
+
+	public BaseContaDto() {
+	}
+
+	public BaseContaDto(int numero, int agencia, int tipo, double saldo, String titular) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.saldo = saldo;
+		this.titular = titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+}
+
+
--- power/src/main/java/com/javabank/power/dto/OperacaoSaldoDto.java
+++ power/src/main/java/com/javabank/power/dto/OperacaoSaldoDto.java
@@ -1,5 +1,21 @@
 package com.javabank.power.dto;
 
 public class OperacaoSaldoDto {
+	private double saldo;
+
+	public OperacaoSaldoDto() {
+	}
+
+	public OperacaoSaldoDto(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 
 }
